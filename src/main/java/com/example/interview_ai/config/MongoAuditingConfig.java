acfd890b2cb1f1b1d5b9838341f27b3d402f5cdc
parkbj12 <164724858+parkbj12@users.noreply.github.com>
2025-11-05 14:42:40 +0900

    Delete src directory

--- src/main/java/com/example/interview_ai/config/MongoAuditingConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.interview_ai.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-
-@Configuration
-@EnableMongoAuditing
-public class MongoAuditingConfig {
-}
